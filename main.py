b2985af3a1f31fb61ed4b84e8a6a3d3004c069f5
AntHaa78 <137605952+AntHaa78@users.noreply.github.com>
2024-02-02 13:33:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None # disable a false positive warning line 116
+import random
 import math
 import json
 import time
@@ -16,8 +18,6 @@ from scipy.sparse.linalg import svds
 
 """ from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
-
-
 import warnings; warnings.simplefilter('ignore')
 %matplotlib inline """
 
@@ -32,7 +32,7 @@ rows, columns = videogames_df.shape
 #print(f"Number of rows: {rows}  | Number of columns: {columns}")
 
 # Taking subset (large dataset)
-videogames_df1=videogames_df.iloc[:3000]
+videogames_df1=videogames_df.iloc[:10000]
 del videogames_df
 #print(videogames_df1.info())
 
@@ -42,6 +42,8 @@ del videogames_df
 
 #checking for missing values
 #print("Number of missing values across columns:\n", videogames_df1.isnull().sum())
+# -> no missing values
+
 
 # rating distribution graphs
 """ print(videogames_df1['ratings'].value_counts())
@@ -61,17 +63,19 @@ print('Number of unique products in videogames data = ', videogames_df1['product
 most_rated=videogames_df1.groupby('userID').size().sort_values(ascending=False)[:10]
 print('Top 10 users based on number of ratings: \n',most_rated)
 
-# Take users that provided > 100 ratings
+# Take users that provided > 200 ratings
 counts = videogames_df1.userID.value_counts()
-videogames_df1_final = videogames_df1[videogames_df1.userID.isin(counts[counts>100].index)]
-print('Number of users that provided more than 100 ratings: ', videogames_df1_final['userID'].nunique())
+videogames_df1_final = videogames_df1[videogames_df1.userID.isin(counts[counts>200].index)]
+print('Number of users that provided more than 200 ratings: ', videogames_df1_final['userID'].nunique())
 print('Number of unique products in final data: ', videogames_df1_final['productID'].nunique())
 print('Number of products in final data: ', len(videogames_df1_final))
 
+# checking top 10 products with most ratings
 #print(videogames_df1_final.groupby("productID").size().sort_values(ascending=False)[:10])
-#construction of pivot table
+
+#construction of pivot table. We have some duplicates users/productID so using the mean of ratings(aggfunc), if a product has no rating from a user -> 0 (fillna0)
 #videogames_df1_final.set_index(['userID', 'productID', 'ratings'], append=True)
-final_ratings_matrix = videogames_df1_final.pivot(index='userID', columns ='productID', values='ratings').fillna(0)
+final_ratings_matrix = videogames_df1_final.pivot_table(index='userID', columns ='productID', values='ratings', aggfunc='mean').fillna(0)
 #print(final_ratings_matrix.head())
 print('Shape of final_ratings_matrix: ', final_ratings_matrix.shape)
 
@@ -82,7 +86,7 @@ num_of_ratings_possible = final_ratings_matrix.shape[0]*final_ratings_matrix.sha
 print("Possible total of ratings: ", num_of_ratings_possible)
 density = (num_of_ratings_given/num_of_ratings_possible)*100
 print(f"The density of the matrix is: {density:.2f}%")
-# -> Sparse matrix, not many products have ratings from different users
+# -> Sparse matrix, unfortunately not many products have ratings from different users
 
 #Split the data randomly into train and test datasets, ratio 70:30
 train_data, test_data = train_test_split(videogames_df1_final, test_size = 0.3, random_state=0)
@@ -93,12 +97,57 @@ print('Shape of testing data: ',test_data.shape)
 # ----------------------------------------------------------------------
 # Popularity recommender model
 
-#Count of user_id for each unique product as a recommendation score
-train_data_grouped = train_data.groupby('productID').agg({'userID': 'count'}).reset_index()
+# Count number of ratings and average rating for each unique product as a recommendation score
+train_data_grouped = train_data.groupby('productID').agg({'userID': 'count','ratings': 'mean'}).reset_index()
+train_data_grouped.rename(columns={'ratings': 'average rating'}, inplace=True)
 train_data_grouped.rename(columns = {'userID': 'score'}, inplace=True)
-#print(train_data_grouped.head(20))
+# Sort the products on based on a score, first number of ratings and then average rating, check top 10
+train_data_sort = train_data_grouped.sort_values(['score', 'average rating'], ascending=False)
+print(train_data_sort.head(10))
+# We see that first 5 products have 4+ reviews and average score >3.75, this will be our 5 recommended products
+train_data_sort['rank'] = train_data_sort['score'].rank(ascending=0, method='first') # add of rank column
+popularity_recommendations = train_data_sort.head(5)
+print(popularity_recommendations)
+print('\n')
 
-# Sort the products on recommendation score
-train_data_sort = train_data_grouped.sort_values(['score', 'productID'], ascending=[0,1])
-train_data_sort['rank'] = train_data_sort['score'].rank(ascending=0, method='first')
-print(train_data_sort.head(5))
+
+#Use of popularity based recommender model to make predictions
+def recommend(user_id):
+    user_recommendations = popularity_recommendations
+    # add user_id column for which the recommendation is generated
+    user_recommendations['userId'] = user_id
+    #bring user_id column to the front
+    cols = user_recommendations.columns.tolist() 
+    cols = cols[-1:] + cols[:-1] 
+    user_recommendations = user_recommendations[cols] 
+          
+    return user_recommendations      
+
+random_users = random.sample(range(1, 500), 3)
+#for i in random_users:  
+#    print("The list of recommendations for the userId: %d\n" %(i))
+#    print(recommend(i)) 
+#    print("\n") 
+
+# Conclusion: We see that every user gets the same recommendations (normal) -> non-personalized recommender model.
+    
+#----------------------------------------------------------------------
+# Collaborative Filtering recommender model
+
+videogames_df_CF = pd.concat([train_data, test_data]).reset_index()
+#print('\n', videogames_df_CF.head())
+
+#Matrix with row per user and column per product
+pivot_df = videogames_df_CF.pivot_table(index='userID', columns='productID', values = 'ratings', aggfunc='mean').fillna(0)
+#print(pivot_df.head())
+print('Shape of the pivot table: ', pivot_df.shape)
+
+# Define user index from 0 to 10
+pivot_df['user_index'] = np.arange(pivot_df.shape[0])
+pivot_df.set_index(['user_index'], inplace=True)
+#print(pivot_df.head())
+# Another sparse matrix, lets use Singular Value Decomposition (SVD)
+
+# Singlular Value Decomposition
+U, sigma, Vt = svds(pivot_df, k=10)
+print("Left singular matrix: \n", U)
